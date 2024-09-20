5dad94a4460e1d59d6d33e6b8e7aec4232b124da
josecarlosgk <josecarlos01galvankamey@gmail.com>
2024-09-20 06:33:30 -0600

    main method

--- LinkedList/App.java
+++ LinkedList/App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
+
         linkedList.insert(1);
         linkedList.insert(2);
         linkedList.insert(3);
+
         linkedList.show();
     }
 }
